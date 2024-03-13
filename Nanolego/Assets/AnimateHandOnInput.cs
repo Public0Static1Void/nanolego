4dd465d51be156f519035511de5d2ef04327074e
Dani <dangp2003@gmail.com>
2024-03-13 21:11:32 +0100

    Movimiento e intento de tp

--- Nanolego/Assets/AnimateHandOnInput.cs
+++ Nanolego/Assets/AnimateHandOnInput.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -14,6 +15,10 @@ public class AnimateHandOnInput : MonoBehaviour
     private int curr_pos = 0;
     private bool activated = false;
 
+    [SerializeField] private TMP_Text trigger_text;
+    [SerializeField] private TMP_Text grip_text;
+    [SerializeField] private TMP_Text room_text;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,11 +30,19 @@ public class AnimateHandOnInput : MonoBehaviour
     {
         float triggerValue = pinchAnimationAction.action.ReadValue<float>();
         handAnimator.SetFloat("Trigger", triggerValue);
+        trigger_text.text = "Trigger:" + triggerValue.ToString();
 
         float gripValue = gripAnimationAction.action.ReadValue<float>();
         handAnimator.SetFloat("Grip", gripValue);
+        grip_text.text = "Grip: " + gripValue.ToString();
 
-        if (triggerValue > 0.9f && !activated)
+        if (room_text != null )
+            room_text.text = "Room: " + curr_pos.ToString();
+
+        if (triggerValue == 1)
+            RoomSystem.instance.current_room++;
+
+        /*if (triggerValue > 0.9f && !activated)
         {
             if (curr_pos < positions.Count)
                 curr_pos++;
@@ -40,9 +53,9 @@ public class AnimateHandOnInput : MonoBehaviour
             if (curr_pos > 0)
                 curr_pos--;
             StartCoroutine(Wait());
-        }
+        }*/
 
-        Vector3.Lerp(transform.position, positions[curr_pos].position, 5);
+        //Vector3.Lerp(transform.position, positions[curr_pos].position, 5);
     }
 
     private IEnumerator Wait()
--- Nanolego/Assets/Scripts/RoomSystem.cs
+++ Nanolego/Assets/Scripts/RoomSystem.cs
@@ -1,16 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class RoomSystem : MonoBehaviour
 {
+    public static RoomSystem instance { get; private set; }
+    [SerializeField] private InputActionProperty rightHandButtons;
+    [SerializeField] private TMP_Text room_text;
     public List<Transform> rooms_transform;
-    private int current_room = 0;
+    public int current_room = 0;
 
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(this);
+    }
     void Update()
     {
-        transform.position = rooms_transform[current_room].position;
+        room_text.text = "Room: " + current_room.ToString();
+        bool btn_pressed = rightHandButtons.action.ReadValue<bool>();
+        if (btn_pressed)
+            current_room++;
     }
 
     public void IncreaseRoom(InputAction.CallbackContext con)
