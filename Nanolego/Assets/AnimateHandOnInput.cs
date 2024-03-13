45754f6980e6552f23776ed48e433d02b4885359
Daniel Gonzalez Peralta <daniel.gonzalez@enti.cat>
2024-03-13 11:17:01 +0100

    Room system

--- Nanolego/Assets/AnimateHandOnInput.cs
+++ Nanolego/Assets/AnimateHandOnInput.cs
@@ -10,10 +10,14 @@ public class AnimateHandOnInput : MonoBehaviour
 
     public Animator handAnimator;
 
+    public List<Transform> positions;
+    private int curr_pos = 0;
+    private bool activated = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        positions = new List<Transform>();
     }
 
     // Update is called once per frame
@@ -24,5 +28,27 @@ public class AnimateHandOnInput : MonoBehaviour
 
         float gripValue = gripAnimationAction.action.ReadValue<float>();
         handAnimator.SetFloat("Grip", gripValue);
+
+        if (triggerValue > 0.9f && !activated)
+        {
+            if (curr_pos < positions.Count)
+                curr_pos++;
+            StartCoroutine(Wait());
+        }
+        if (gripValue > 0.9f && !activated)
+        {
+            if (curr_pos > 0)
+                curr_pos--;
+            StartCoroutine(Wait());
+        }
+
+        Vector3.Lerp(transform.position, positions[curr_pos].position, 5);
+    }
+
+    private IEnumerator Wait()
+    {
+        activated = true;
+        yield return new WaitForSeconds(0.2f);
+        activated = false;
     }
 }
