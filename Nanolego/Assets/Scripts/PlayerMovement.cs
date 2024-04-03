2795b7b9e41f39a5df789a8bc3ea4d1cd7ccb0b8
Dani <dangp2003@gmail.com>
2024-04-03 20:48:24 +0200

    Tira pa alante bien

--- Nanolego/Assets/Scripts/PlayerMovement.cs
+++ Nanolego/Assets/Scripts/PlayerMovement.cs
@@ -12,6 +12,14 @@ public class PlayerMovement : MonoBehaviour
     {
         chController = GetComponent<CharacterController>();
     }
+
+    private void Update()
+    {
+        chController.Move(new Vector3 (Input.GetAxis("Horizontal") * speed * transform.forward.x, 0, 
+                                        Input.GetAxis("Vertical") * speed * transform.forward.y));
+        Debug.Log("Horizontal: " + Input.GetAxis("Horizontal"));
+        Debug.Log("Vertical: " + Input.GetAxis("Vertical"));
+    }
     public void Move(InputAction.CallbackContext con)
     {
         if (con.performed)
